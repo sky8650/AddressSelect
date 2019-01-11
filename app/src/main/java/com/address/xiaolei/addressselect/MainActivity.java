0bb6c0dda5c4af60be2fe1d686d1281620dbe798
xiaolei <865079106@qq.com>
2019-01-11 16:17:10 +0800

    commit

--- app/src/main/java/com/address/xiaolei/addressselect/MainActivity.java
+++ app/src/main/java/com/address/xiaolei/addressselect/MainActivity.java
@@ -1,18 +1,25 @@
 package com.address.xiaolei.addressselect;
 
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.address.xiaolei.addressselect.adapter.PickCityAdapter;
 import com.address.xiaolei.addressselect.sqlutil.DBManager;
 import com.address.xiaolei.addressselect.utils.RxUtils;
+import com.address.xiaolei.addressselect.view.SlideBar;
 import com.address.xiaolei.addressselect.vo.CityVo;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.Observable;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 
@@ -21,38 +28,60 @@ import io.reactivex.functions.Function;
  */
 public class MainActivity extends AppCompatActivity {
     DBManager dbManager;
-    SQLiteDatabase  sqLiteDatabase;
+    SQLiteDatabase sqLiteDatabase;
+    @BindView(R.id.iv_clear)
+    ImageView ivClear;
+    @BindView(R.id.et_city_name)
+    EditText etCityName;
+    @BindView(R.id.ll_search)
+    RelativeLayout llSearch;
+    @BindView(R.id.rv_city)
+    RecyclerView rvCity;
+    @BindView(R.id.slideBar)
+    SlideBar slideBar;
+
+    PickCityAdapter  pickCityAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initView();
         initDb();
+    }
+
+
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+
     }
 
 
     /**
      * 初始化数据库
      */
-    private void  initDb(){
+    private void initDb() {
         dbManager = new DBManager(MainActivity.this);
         Observable.just(dbManager).map(new Function<DBManager, Object>() {
-         @Override
-         public Object apply(DBManager dbManager) throws Exception {
-             sqLiteDatabase = dbManager.initDataBase(getPackageName());
-             String[] columns = new String[]//列属性
-                     {"cityType", "cityID", "cityName", "parentId"};
-             String selection = "cityType=?";
-             String[] selectionArgs = new String[]{"3"};//type为3时查询的是市
-             List<CityVo> cityVoList = dbManager.query(sqLiteDatabase, columns, selection, selectionArgs);
-             sqLiteDatabase.close();
-             return cityVoList;
-         }
-     }).compose(RxUtils.schedulersTransformer()).subscribe(new Consumer<List<CityVo>>() {
-         @Override
-         public void accept(List<CityVo> cityVoList) throws Exception {
-             Log.d("******",cityVoList.size()+"");
-         }
-     });
+            @Override
+            public Object apply(DBManager dbManager) throws Exception {
+                sqLiteDatabase = dbManager.initDataBase(getPackageName());
+                String[] columns = new String[]//列属性
+                        {"cityType", "cityID", "cityName", "parentId"};
+                String selection = "cityType=?";
+                String[] selectionArgs = new String[]{"3"};//type为3时查询的是市
+                List<CityVo> cityVoList = dbManager.query(sqLiteDatabase, columns, selection, selectionArgs);
+                sqLiteDatabase.close();
+                return cityVoList;
+            }
+        }).compose(RxUtils.schedulersTransformer()).subscribe(new Consumer<List<CityVo>>() {
+            @Override
+            public void accept(List<CityVo> cityVoList) throws Exception {
+                Log.d("******", cityVoList.size() + "");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/address/xiaolei/addressselect/adapter/PickCityAdapter.java
@@ -0,0 +1,45 @@
+package com.address.xiaolei.addressselect.adapter;
+
+import android.view.View;
+
+import com.address.xiaolei.addressselect.R;
+import com.address.xiaolei.addressselect.vo.CityVo;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+
+/**
+ * Created by Cicinnus on 2017/6/8.
+ */
+
+public class PickCityAdapter extends BaseQuickAdapter<CityVo, BaseViewHolder> {
+
+
+    private OnCityClickListener onCityClickListener;
+
+    public PickCityAdapter() {
+        super(R.layout.item_city);
+    }
+    @Override
+    protected void convert(BaseViewHolder helper, final CityVo item) {
+        helper.setText(R.id.tv_city_name, item.getCityName());
+        helper.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onCityClickListener != null) {
+                    //onCityClickListener.onClick(item);
+                }
+            }
+        });
+    }
+
+    public void setOnCityClickListener(OnCityClickListener onCityClickListener) {
+        this.onCityClickListener = onCityClickListener;
+    }
+
+    public interface OnCityClickListener{
+        void onClick(CityVo item);
+    }
+
+
+}
