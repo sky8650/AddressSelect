c838e8b5ea686ab6e27b4a37227c83522527300f
xiaolei <865079106@qq.com>
2019-01-11 15:37:23 +0800

    布局上传

--- app/src/main/java/com/address/xiaolei/addressselect/MainActivity.java
+++ app/src/main/java/com/address/xiaolei/addressselect/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.address.xiaolei.addressselect.sqlutil.DBManager;
+import com.address.xiaolei.addressselect.utils.RxUtils;
 import com.address.xiaolei.addressselect.vo.CityVo;
 
 import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 /**
  * xiaolei
@@ -29,22 +35,24 @@ public class MainActivity extends AppCompatActivity {
      * 初始化数据库
      */
     private void  initDb(){
-
-
-
-
-
-        dbManager = new DBManager(this);
-        sqLiteDatabase = dbManager.initDataBase(getPackageName());
-        String[] columns = new String[]//列属性
-                {"cityType", "cityID", "cityName", "parentId"};
-        String selection = "cityType=?";
-        String[] selectionArgs = new String[]{"3"};//type为3时查询的是市
-        List<CityVo> cityVoList = dbManager.query(sqLiteDatabase, columns, selection, selectionArgs);
-        sqLiteDatabase.close();
-        Log.d("******",cityVoList.size()+"");
-
-
-
+        dbManager = new DBManager(MainActivity.this);
+        Observable.just(dbManager).map(new Function<DBManager, Object>() {
+         @Override
+         public Object apply(DBManager dbManager) throws Exception {
+             sqLiteDatabase = dbManager.initDataBase(getPackageName());
+             String[] columns = new String[]//列属性
+                     {"cityType", "cityID", "cityName", "parentId"};
+             String selection = "cityType=?";
+             String[] selectionArgs = new String[]{"3"};//type为3时查询的是市
+             List<CityVo> cityVoList = dbManager.query(sqLiteDatabase, columns, selection, selectionArgs);
+             sqLiteDatabase.close();
+             return cityVoList;
+         }
+     }).compose(RxUtils.schedulersTransformer()).subscribe(new Consumer<List<CityVo>>() {
+         @Override
+         public void accept(List<CityVo> cityVoList) throws Exception {
+             Log.d("******",cityVoList.size()+"");
+         }
+     });
     }
 }
